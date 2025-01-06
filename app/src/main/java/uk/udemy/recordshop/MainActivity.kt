7cabc04f82cf0d70e0235833c7dc58301f2387ce
Tawanda Chabva <tchabva@gmail.com>
2025-01-06 22:03:54 +0000

    feat: force implementation of coroutines

--- app/src/main/java/uk/udemy/recordshop/MainActivity.kt
+++ app/src/main/java/uk/udemy/recordshop/MainActivity.kt
@@ -1,28 +1,46 @@
 package uk.udemy.recordshop
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.runBlocking
+import uk.udemy.recordshop.model.Album
+import uk.udemy.recordshop.model.service.albumService
+import uk.udemy.recordshop.ui.home.HomeScreen
 import uk.udemy.recordshop.ui.theme.RecordShopTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var albums: List<Album> = emptyList()
+
+        runBlocking {
+            try{
+                albums = albumService.getAllInStockAlbums()
+            }catch (e : Exception){
+                albums = emptyList()
+                Log.e("Error", e.message.toString())
+            }
+        }
+
         enableEdgeToEdge()
         setContent {
+
+
             RecordShopTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    HomeScreen(
+                        paddingValues = innerPadding,
+                        albums = albums
                     )
                 }
             }
