77199bb35621ae8c2e1ae29dfec14c64d2736a93
Tawanda Chabva <tchabva@gmail.com>
2025-02-04 13:30:16 +0000

    build: move Hilt dependencies to libs file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,9 +74,9 @@ dependencies {
     implementation(libs.compose.glide)
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.52")
-    kapt("com.google.dagger:hilt-android-compiler:2.52")
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
 }
 
 kapt {
