23647c6889ebe34112b7bc0ce64ea83f62d8e26c
Tawanda Chabva <tchabva@gmail.com>
2025-02-04 15:59:43 +0000

    refactor: changed AlbumApiService to RecordShopApiService

--- app/src/main/java/uk/udemy/recordshop/data/RecordShopApiService.kt
+++ app/src/main/java/uk/udemy/recordshop/data/RecordShopApiService.kt
@@ -3,7 +3,7 @@ package uk.udemy.recordshop.data
 import retrofit2.http.GET
 import uk.udemy.recordshop.model.Album
 
-interface AlbumApiService {
+interface RecordShopApiService {
 
     @GET("albums")
     suspend fun getAllInStockAlbums(): List<Album>
