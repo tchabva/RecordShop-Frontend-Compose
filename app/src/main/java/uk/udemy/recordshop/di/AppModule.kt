32526de42b4d55dce0850c63365367b3221a0539
Tawanda Chabva <tchabva@gmail.com>
2025-02-04 16:07:01 +0000

    feat: create di package
    with a AppModule object with Hilt annotatations for a singleton to create the instance of the Record API service

---
+++ app/src/main/java/uk/udemy/recordshop/di/AppModule.kt
@@ -0,0 +1,34 @@
+package uk.udemy.recordshop.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import uk.udemy.recordshop.data.RecordShopApiService
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    private const val BASE_URL_RECORDS = "http://192.168.50.167:8080/api/v1/"
+    private val interceptorAlbums = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    private val clientAlbums = OkHttpClient.Builder().addInterceptor(interceptorAlbums).build()
+
+
+    @Provides
+    @Singleton
+    fun provideAlbumApi(): RecordShopApiService {
+        return Retrofit
+            .Builder()
+            .baseUrl(BASE_URL_RECORDS)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(clientAlbums)
+            .build()
+            .create(RecordShopApiService::class.java)
+    }
+}
