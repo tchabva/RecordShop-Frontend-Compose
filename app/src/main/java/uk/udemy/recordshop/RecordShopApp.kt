15abec9a3d93076cc52f3448e00f36a57b7ad2dd
Tawanda Chabva <tchabva@gmail.com>
2025-02-05 16:12:36 +0000

    feat: HiltAndroidApp
    update the RecordShop file to a class extending Application, with HiltAndroidApp annotation and added name to the Manifest

--- app/src/main/java/uk/udemy/recordshop/RecordShopApp.kt
+++ app/src/main/java/uk/udemy/recordshop/RecordShopApp.kt
@@ -1,20 +1,7 @@
 package uk.udemy.recordshop
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
-@Composable
-fun RecordShopApp(paddingValues: PaddingValues, navHostController: NavHostController){
-
-    val viewModel: MainViewModel = viewModel()
-    val viewState by viewModel.recordShopState
-
-    /*
-    TODO : FINISH IMPLEMENTING HOMESCREEN USING NAVHOST
-     */
-
-
-}
+@HiltAndroidApp
+class RecordShopApp: Application()
