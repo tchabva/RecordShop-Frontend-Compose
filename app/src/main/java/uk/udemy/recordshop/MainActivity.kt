99e05fc8fcc1a0c7c18b08a86b00e06577004f6a
Tawanda Chabva <tchabva@gmail.com>
2025-02-06 15:44:42 +0000

    refactor: removed redundant greeting and preview code

--- app/src/main/java/uk/udemy/recordshop/MainActivity.kt
+++ app/src/main/java/uk/udemy/recordshop/MainActivity.kt
@@ -6,12 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
-import uk.udemy.recordshop.data.model.Album
 import uk.udemy.recordshop.ui.home.HomeScreen
 import uk.udemy.recordshop.ui.theme.RecordShopTheme
 
@@ -31,21 +27,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecordShopTheme {
-        Greeting("Android")
     }
 }
