ad0cd199d3017640f6b213cd3c1c1caedabeffb8
Tawanda Chabva <tchabva@gmail.com>
2025-02-06 21:47:20 +0000

    feat: add Logging and navigateToDetailMethod to HomeViewModel

--- app/src/main/java/uk/udemy/recordshop/ui/home/HomeViewModel.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package uk.udemy.recordshop.ui.home
 
+import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
@@ -8,7 +9,6 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import uk.udemy.recordshop.data.remote.Result
 import uk.udemy.recordshop.data.repository.RecordsRepository
-import uk.udemy.recordshop.data.model.Album
 import javax.inject.Inject
 
 @HiltViewModel
@@ -27,32 +27,14 @@ class HomeViewModel @Inject constructor(
     private fun getAlbums(){
         viewModelScope.launch {
 
-//            try {
-//                val response = albumService.getAllAlbums()
-//                _recordShopState.value = _recordShopState.value.copy(
-//                    loading = false,
-//                    list = response.body() ?: emptyList(),
-//                    error = null
-//                )
-//            }catch (e: Exception){
-//                _recordShopState.value = _recordShopState.value.copy(
-//                    loading = false,
-//                    error = "Error Loading albums ${e.message}"
-//                )
-//            }
             when (val networkResponse = repository.getAllAlbums()){
-//                is NetworkResponse.Exception -> {
-//                    _homeScreenState.value = HomeScreenState.Error()
-//                }
-//                is NetworkResponse.Failure -> {
-//                    _homeScreenState.value = HomeScreenState.Error()
-//                }
-//                is NetworkResponse.Success -> {
-//                    _homeScreenState.value = HomeScreenState.Loaded(networkResponse.data)
-//                }
 
                 is Result.Exception -> {
-                    _homeScreenState.value = _homeScreenState.value.copy(isLoading = false)
+                    _homeScreenState.value = _homeScreenState.value.copy(
+                        isLoading = false,
+                        error = networkResponse.exception.message
+                    )
+
                 }
                 is Result.Failed -> {
                     _homeScreenState.value = _homeScreenState.value.copy(
@@ -68,5 +50,13 @@ class HomeViewModel @Inject constructor(
                 }
             }
         }
+    }
+
+    fun navigateToAlbumDetail(albumId: Long){
+        Log.i(TAG, "Clicked on Album with the Id $albumId")
+    }
+
+    companion object {
+        private const val TAG = "HomeViewModel"
     }
 }
