4dd84f06e7b94f6f9cdb9565db270b44b5fe8448
Tawanda Chabva <tchabva@gmail.com>
2025-02-07 16:23:33 +0000

    feat: add routes to the Screen file

---
+++ app/src/main/java/uk/udemy/recordshop/ui/navigation/Screen.kt
@@ -0,0 +1,28 @@
+package uk.udemy.recordshop.ui.navigation
+
+import kotlinx.serialization.Serializable
+import java.io.Serial
+
+/*
+Note: This ensures that we are always consistent route names for the Screens
+ */
+@Serializable
+object Home
+
+@Serializable
+object Artists
+
+@Serializable
+object Genres
+
+@Serializable
+data class Artist(val artistId: Long)
+
+@Serializable
+data class Genre(val genreId: Long)
+
+@Serializable
+data class ViewAlbum(val albumId: Long)
+
+@Serializable
+data class AddOrEditAlbum(val albumId: Long? = null)
