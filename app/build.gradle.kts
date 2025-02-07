d7e318d90303cbad6cc5fef5fa78e8620c26bbd8
Tawanda Chabva <tchabva@gmail.com>
2025-02-07 15:22:17 +0000

    build: update targetSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "uk.udemy.recordshop"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
