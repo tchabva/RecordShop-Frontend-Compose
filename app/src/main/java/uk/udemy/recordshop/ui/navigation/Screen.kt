179b676f2144922de56bd38831b362ed9e3540af
Tawanda Chabva <tchabva@gmail.com>
2025-02-09 17:45:35 +0000

    feat: added sealed Tabs and Screens interfaces for the nested Nav Graphs and the Composable Screens

--- app/src/main/java/uk/udemy/recordshop/ui/navigation/Screen.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/navigation/Screen.kt
@@ -5,23 +5,47 @@ import kotlinx.serialization.Serializable
 /*
 Note: This ensures that we are always consistent route names for the Screens
  */
+// Tabs are for the sub navigation graphs
 @Serializable
-object Home
+sealed interface Tabs {
+    @Serializable
+    data object Home : Tabs
 
-@Serializable
-object Artists
+    @Serializable
+    data object Artists : Tabs
 
-@Serializable
-object Genres
+    @Serializable
+    data object Genres : Tabs
+}
 
+// The Screens are for the each composable Screen
 @Serializable
-data class Artist(val artistId: Long)
+sealed interface Screens {
+    @Serializable
+    data object Home : Screens
 
-@Serializable
-data class Genre(val genreId: Long)
+    @Serializable
+    data object Artists : Screens
 
-@Serializable
-data class ViewAlbum(val albumId: Long)
+    @Serializable
+    data object Genres : Screens
 
-@Serializable
-data class AddOrEditAlbum(val albumId: Long? = null)
+    @Serializable
+    data class Artist(val artistId: Long) : Screens
+
+    @Serializable
+    data class Genre(val genreId: Long) : Screens
+
+    @Serializable
+    data class ViewAlbum(val albumId: Long) : Screens
+
+    @Serializable
+    data class AddOrEditAlbum(val albumId: Long? = null) : Screens
+
+    companion object{
+        val screensWithoutBottomNav = listOf(
+            ViewAlbum::class,
+            AddOrEditAlbum::class
+        )
+    }
+}
