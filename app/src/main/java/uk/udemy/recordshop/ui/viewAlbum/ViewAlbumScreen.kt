10647d8b24edf1e09569900d8912bb3c2663ab77
Tawanda Chabva <tchabva@gmail.com>
2025-02-09 21:44:02 +0000

    feat: create placeholder ViewAlbumScreen

---
+++ app/src/main/java/uk/udemy/recordshop/ui/viewAlbum/ViewAlbumScreen.kt
@@ -0,0 +1,18 @@
+package uk.udemy.recordshop.ui.viewAlbum
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ViewAlbumScreen(){
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text("View Album Screen")
+    }
+}
