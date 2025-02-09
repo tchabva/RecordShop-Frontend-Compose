ade0720dc81fc8fa3da72f8f5f4614b428ad6b0d
Tawanda Chabva <tchabva@gmail.com>
2025-02-09 18:05:19 +0000

    feat: implement the sub NavGraphs

--- app/src/main/java/uk/udemy/recordshop/ui/navigation/NavigationGraph.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/navigation/NavigationGraph.kt
@@ -1,28 +1,46 @@
 package uk.udemy.recordshop.ui.navigation
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
-import uk.udemy.recordshop.ui.artists.ArtistsScreen
-import uk.udemy.recordshop.ui.genres.GenresScreen
+import artistGraph
+import uk.udemy.recordshop.ui.addOrEditAlbum.AddOrEditAlbumScreen
 
+// The Root NavGraph
 @Composable
 fun NavigationGraph(
     modifier: Modifier,
     navController: NavHostController,
-    startDestination: Any = Home,
+    startDestination: Any = Tabs.Home,
     innerPadding: PaddingValues
 ) = NavHost(
-    modifier = modifier,
+    modifier = modifier.padding(innerPadding),
     navController = navController,
     startDestination = startDestination
 
 ){
 
-    homeGraph(innerPadding)
-    composable<Artists>{ ArtistsScreen(innerPadding) }
-    composable<Genres>{ GenresScreen(innerPadding) }
+    // The nested Home Tab NavGraph
+    homeGraph(
+        navController = navController
+    )
+
+    // The nested Artists Tab NavGraph
+    artistGraph(
+        navController = navController
+    )
+
+    // The nested Genres Tab NavGraph
+    genresGraph(
+        navController = navController
+    )
+
+    // For navigating to the Add Album Screen from the HomeTab
+    composable<Screens.AddOrEditAlbum> {
+        AddOrEditAlbumScreen()
+    }
 }
