836a7e4aabdc131b29752d15d7b71a9d19d0761e
Tawanda Chabva <tchabva@gmail.com>
2025-02-09 18:01:48 +0000

    refactor: removed paddingValues param

--- app/src/main/java/uk/udemy/recordshop/ui/artists/ArtistsScreen.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/artists/ArtistsScreen.kt
@@ -1,20 +1,17 @@
 package uk.udemy.recordshop.ui.artists
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun ArtistsScreen(paddingValues: PaddingValues){
+fun ArtistsScreen(){
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(paddingValues)
     ) {
         Text("Artists Screen")
     }
