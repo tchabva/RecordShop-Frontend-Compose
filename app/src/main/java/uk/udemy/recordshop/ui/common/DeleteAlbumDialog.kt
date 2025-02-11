58196144893a3404f0544679274992302e464605
Tawanda Chabva <tchabva@gmail.com>
2025-02-11 20:38:52 +0000

    refactor: updated Button texts, sizes and colour

--- app/src/main/java/uk/udemy/recordshop/ui/common/DeleteAlbumDialog.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/common/DeleteAlbumDialog.kt
@@ -1,11 +1,15 @@
 package uk.udemy.recordshop.ui.common
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import uk.udemy.recordshop.R
@@ -30,20 +34,25 @@ fun DeleteAlbumDialog(
         },
         confirmButton = {
             Button(
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Red
+                ),
                 onClick = {
                     onDeleteAlbumConfirmed()
                 }
             ) {
-                Text(stringResource(R.string.confirm))
+                Text(stringResource(R.string.yes))
             }
         },
         dismissButton = {
             Button(
+                modifier = Modifier.fillMaxWidth(),
                 onClick = {
                     onDismiss()
                 }
             ) {
-                Text(stringResource(R.string.cancel))
+                Text(stringResource(R.string.no))
             }
         }
     )
