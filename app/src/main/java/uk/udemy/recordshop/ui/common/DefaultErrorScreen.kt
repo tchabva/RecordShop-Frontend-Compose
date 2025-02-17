47dd125a725c6a957d7914af958c66aa4eda1bfa
Tawanda Chabva <tchabva@gmail.com>
2025-02-17 14:21:19 +0000

    feat: create DefaultErorrScreen file

---
+++ app/src/main/java/uk/udemy/recordshop/ui/common/DefaultErrorScreen.kt
@@ -0,0 +1,27 @@
+package uk.udemy.recordshop.ui.common
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import uk.udemy.recordshop.R
+
+@Composable
+fun DefaultErrorScreen(responseCode: Int, errorMessage: String?) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = stringResource(
+                R.string.error_occurred,
+                responseCode,
+                errorMessage ?: stringResource(R.string.unknown_error)
+            )
+        )
+    }
+}
