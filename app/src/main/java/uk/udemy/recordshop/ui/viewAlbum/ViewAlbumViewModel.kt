ed1c37868ff6815ea4260dd2264ab79f6df2b48b
Tawanda Chabva <tchabva@gmail.com>
2025-02-26 21:31:20 +0000

    refactor: moved State to ViewModel, add Events and methods for Dialog

--- app/src/main/java/uk/udemy/recordshop/ui/viewAlbum/ViewAlbumViewModel.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/viewAlbum/ViewAlbumViewModel.kt
@@ -1,13 +1,15 @@
 package uk.udemy.recordshop.ui.viewAlbum
 
 import android.util.Log
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import uk.udemy.recordshop.data.model.Album
 import uk.udemy.recordshop.data.remote.NetworkResponse
 import uk.udemy.recordshop.data.repository.RecordsRepository
 import javax.inject.Inject
@@ -16,30 +18,34 @@ import javax.inject.Inject
 class ViewAlbumViewModel @Inject constructor(
     private val repository: RecordsRepository
 ) : ViewModel() {
-    private val _viewAlbumScreenState: MutableState<ViewAlbumScreenState> =
-        mutableStateOf(ViewAlbumScreenState.Loading)
-    val viewAlbumScreenState: State<ViewAlbumScreenState> = _viewAlbumScreenState
 
+    private val _state: MutableStateFlow<State> = MutableStateFlow(State.Loading)
+    val state: StateFlow<State> = _state
+
+    private val _events: MutableSharedFlow<Event> = MutableSharedFlow()
+    val events: SharedFlow<Event> = _events
+
+    // Retrieves the Album from the Backend using the Album Id
     suspend fun getAlbumById(albumId: Long) {
         when (val networkResponse = repository.getAlbumById(albumId)) {
             is NetworkResponse.Exception -> {
-                _viewAlbumScreenState.value =
-                    ViewAlbumScreenState.NetworkError(
+                _state.value =
+                    State.NetworkError(
                         error = networkResponse.exception.message ?: ""
                     )
             }
 
             is NetworkResponse.Failed -> {
-                _viewAlbumScreenState.value =
-                    ViewAlbumScreenState.Error(
+                _state.value =
+                    State.Error(
                         responseCode = networkResponse.code!!,
                         error = networkResponse.message
                     )
             }
 
             is NetworkResponse.Success -> {
-                _viewAlbumScreenState.value =
-                    ViewAlbumScreenState.Loaded(
+                _state.value =
+                    State.Loaded(
                         data = networkResponse.data
                     )
             }
@@ -47,32 +53,80 @@ class ViewAlbumViewModel @Inject constructor(
     }
 
     fun deleteAlbum(albumId: Long) {
-        _viewAlbumScreenState.value = ViewAlbumScreenState.Loading
+        _state.value = (state.value as State.Loaded).copy(
+            isLoading = true
+        )
 
         viewModelScope.launch {
             when (val networkResponse = repository.deleteAlbumById(albumId)) {
                 is NetworkResponse.Exception -> {
-                    _viewAlbumScreenState.value =
-                        ViewAlbumScreenState.NetworkError(
-                            error = networkResponse.exception.message ?: ""
+                    _state.value = (state.value as State.Loaded).copy(
+                        isLoading = false
+                    )
+
+                    emitEvent(
+                        Event.DeleteAlbumFailed(
+                            responseCode = null,
+                            error = networkResponse.exception.message ?: "Unknown Error"
                         )
+                    )
                 }
 
                 is NetworkResponse.Failed -> {
-                    _viewAlbumScreenState.value =
-                        ViewAlbumScreenState.Error(
+                    _state.value = (state.value as State.Loaded).copy(
+                        isLoading = false
+                    )
+
+                    emitEvent(
+                        Event.DeleteAlbumFailed(
                             responseCode = networkResponse.code!!,
-                            error = networkResponse.message
+                            error = networkResponse.message ?: "Unknown Error"
                         )
+                    )
                 }
 
                 is NetworkResponse.Success -> {
-                    _viewAlbumScreenState.value =
-                        ViewAlbumScreenState.AlbumDeleted
+                    emitEvent(Event.DeleteAlbumSuccessful)
+                    Log.i(TAG, "Deleted Album of ID: $albumId")
                 }
             }
         }
-        Log.i(TAG, "Deleted Album of ID: $albumId")
+    }
+
+    fun showDeleteAlbumDialog(){
+        _state.value = (state.value as State.Loaded).copy(
+            showDeleteAlbumDialog = true
+        )
+    }
+
+    fun dismissDeleteAlbumDialog(){
+        _state.value = (state.value as State.Loaded).copy(
+            showDeleteAlbumDialog = false
+        )
+    }
+
+    private suspend fun emitEvent(event: Event) {
+        _events.emit(event)
+    }
+
+    sealed interface State {
+        data object Loading : State
+
+        data class Loaded(
+            val data: Album,
+            var isLoading: Boolean = false,
+            var showDeleteAlbumDialog: Boolean = false
+        ) : State
+
+        data class Error(val responseCode: Int, val error: String?) : State
+
+        data class NetworkError(val error: String?) : State
+    }
+
+    sealed interface Event {
+        data class EditAlbumFabClicked(val albumId: Long) : Event
+        data object DeleteAlbumSuccessful : Event
+        data class DeleteAlbumFailed(val responseCode: Int?, val error: String) : Event
     }
 
     companion object {
