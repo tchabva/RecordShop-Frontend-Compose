027d2f747cd78cb6619d72f9aabb527e72fc8be3
Tawanda Chabva <tchabva@gmail.com>
2025-04-02 15:02:29 +0100

    feat: implement ArtistRepositoryImpl getAllArtists method code

--- app/src/main/java/uk/udemy/recordshop/data/repository/ArtistsRepositoryImpl.kt
+++ app/src/main/java/uk/udemy/recordshop/data/repository/ArtistsRepositoryImpl.kt
@@ -1,4 +1,42 @@
 package uk.udemy.recordshop.data.repository
 
-class ArtistsRepositoryImpl {
+import android.util.Log
+import uk.udemy.recordshop.data.model.ArtistAndAlbums
+import uk.udemy.recordshop.data.model.ArtistDTO
+import uk.udemy.recordshop.data.remote.ArtistsApi
+import uk.udemy.recordshop.data.remote.NetworkResponse
+import javax.inject.Inject
+
+class ArtistsRepositoryImpl @Inject constructor(
+    private val api: ArtistsApi,
+) : ArtistsRepository {
+
+    override suspend fun getAllArtists(): NetworkResponse<List<ArtistDTO>> {
+        try {
+            val response = api.getAllArtists()
+            val responseCode = response.code()
+
+            return if (responseCode == 200) {
+                Log.i(TAG, "Successful Retrieval of Artists: ${response.body()!!.size} Artists")
+                NetworkResponse.Success(response.body()!!)
+            } else {
+                Log.e(TAG, "Failed Retrieval of Artists: Code = $responseCode")
+                NetworkResponse.Failed(
+                    message = response.message() ?: "",
+                    code = responseCode
+                )
+            }
+        }catch (e: Throwable) {
+            Log.wtf(TAG, "Network Error", e)
+            return NetworkResponse.Exception(e)
+        }
+    }
+
+    override suspend fun getArtistWithAlbums(): NetworkResponse<ArtistAndAlbums> {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        private const val TAG = "ArtistsRepoImpl"
+    }
 }
