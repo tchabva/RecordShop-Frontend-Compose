b35831a64dda371a0a9ed49720983f4c48e95eee
Tawanda Chabva <tchabva@gmail.com>
2025-04-02 22:59:05 +0100

    feat: implement the functionality for the Artists using the ViewModel

--- app/src/main/java/uk/udemy/recordshop/ui/artists/ArtistsScreen.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/artists/ArtistsScreen.kt
@@ -2,17 +2,42 @@ package uk.udemy.recordshop.ui.artists
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.flow.collect
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ArtistsScreen(){
+fun ArtistsScreen(
+    viewModel: ArtistsViewModel,
+    onArtistItemClicked: (Long) -> Unit
+) {
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Text("Artists Screen")
+    val state = viewModel.state.collectAsStateWithLifecycle()
+    val pullToRefreshState = rememberPullToRefreshState()
+    val onRefresh: () -> Unit = {
+        viewModel.getArtists()
     }
+
+    // Events Observer
+    LaunchedEffect(Unit) {
+        viewModel.events.collect{ event ->
+            when (event){
+                is ArtistsViewModel.Event.ArtistItemClicked -> onArtistItemClicked(event.artistId)
+            }
+
+        }
+    }
+
+    ArtistsScreenContent(
+        state = state.value,
+        pullToRefreshState = pullToRefreshState,
+        onRefresh = onRefresh,
+        onArtistItemClick = viewModel::onArtistItemClicked
+    )
 }
--- app/src/main/java/uk/udemy/recordshop/ui/artists/ArtistsScreenContent.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/artists/ArtistsScreenContent.kt
@@ -1,2 +1,68 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package uk.udemy.recordshop.ui.artists
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshState
+import androidx.compose.material3.pulltorefresh.pullToRefresh
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import uk.udemy.recordshop.ui.common.DefaultErrorScreen
+import uk.udemy.recordshop.ui.common.DefaultNetworkErrorScreen
+import uk.udemy.recordshop.ui.common.DefaultProgressIndicator
+
+@Composable
+fun ArtistsScreenContent(
+    state: ArtistsViewModel.State,
+    pullToRefreshState: PullToRefreshState,
+    onRefresh: () -> Unit,
+    onArtistItemClick: (Long) -> Unit
+) {
+    when(state) {
+        is ArtistsViewModel.State.Error -> {
+            DefaultErrorScreen(
+                responseCode = state.responseCode ?: 0,
+                errorMessage = state.errorMessage
+            )
+        }
+        is ArtistsViewModel.State.Loaded -> {
+            ArtistsScreenLoaded(
+                state = state,
+                pullToRefreshState = pullToRefreshState,
+                onRefresh = onRefresh,
+                onArtistItemClick = onArtistItemClick
+            )
+        }
+        ArtistsViewModel.State.Loading -> {
+            DefaultProgressIndicator()
+        }
+        is ArtistsViewModel.State.NetworkError -> {
+            DefaultNetworkErrorScreen(
+                errorMessage = state.errorMessage
+            )
+        }
+    }
+}
+
+@Composable
+fun ArtistsScreenLoaded(
+    state: ArtistsViewModel.State.Loaded,
+    pullToRefreshState: PullToRefreshState,
+    onRefresh: () -> Unit,
+    onArtistItemClick: (Long) -> Unit
+){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pullToRefresh(
+                isRefreshing = state.isLoading,
+                state = pullToRefreshState,
+                onRefresh = onRefresh
+            ),
+    ){
+        Text("Artist Screen using ViewModel")
+    }
+}
