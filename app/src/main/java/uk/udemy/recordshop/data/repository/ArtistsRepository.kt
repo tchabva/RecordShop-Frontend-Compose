c9abeae0c7ed063477b67598eaf2ab6f70cafbc4
Tawanda Chabva <tchabva@gmail.com>
2025-04-02 13:15:23 +0100

    feat: add methods to the Artists Repository interface

--- app/src/main/java/uk/udemy/recordshop/data/repository/ArtistsRepository.kt
+++ app/src/main/java/uk/udemy/recordshop/data/repository/ArtistsRepository.kt
@@ -1,4 +1,10 @@
 package uk.udemy.recordshop.data.repository
 
+import uk.udemy.recordshop.data.model.ArtistAndAlbums
+import uk.udemy.recordshop.data.model.ArtistDTO
+import uk.udemy.recordshop.data.remote.NetworkResponse
+
 interface ArtistsRepository {
+    suspend fun getAllArtists(): NetworkResponse<List<ArtistDTO>>
+    suspend fun getArtistWithAlbums(): NetworkResponse<ArtistAndAlbums>
 }
