bca82cdb0d4332ed5d470f0abf5ff29c173e1d0e
Tawanda Chabva <tchabva@gmail.com>
2025-04-28 23:04:50 +0100

    feat: update the icon for Genre screen in the BottomNav

--- app/src/main/java/uk/udemy/recordshop/ui/navigation/TopLevelRoute.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/navigation/TopLevelRoute.kt
@@ -2,15 +2,15 @@ package uk.udemy.recordshop.ui.navigation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.MusicNote
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.ui.graphics.vector.ImageVector
 
 
 data class TopLevelRoute<T: Any> (val name: String, val route: T, val icon: ImageVector)
-
+// TODO add clarity for which icon is selected
 val topLevelRoute = listOf(
     TopLevelRoute("Home", Tabs.Home, Icons.Default.Home),
     TopLevelRoute("Artists", Tabs.Artists, Icons.Default.Person),
-    TopLevelRoute("Genres", Tabs.Genres, Icons.Default.Info),
+    TopLevelRoute("Genres", Tabs.Genres, Icons.Default.MusicNote),
 )
