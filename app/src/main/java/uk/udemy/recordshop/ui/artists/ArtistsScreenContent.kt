024266e169485ccb415ca717b1b8feab7ceee4fe
Tawanda Chabva <tchabva@gmail.com>
2025-04-28 22:04:31 +0100

    feat: implement hte LazyColum for the Artists

--- app/src/main/java/uk/udemy/recordshop/ui/artists/ArtistsScreenContent.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/artists/ArtistsScreenContent.kt
@@ -4,14 +4,18 @@ package uk.udemy.recordshop.ui.artists
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshState
 import androidx.compose.material3.pulltorefresh.pullToRefresh
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import uk.udemy.recordshop.ui.common.ArtistItem
 import uk.udemy.recordshop.ui.common.DefaultErrorScreen
 import uk.udemy.recordshop.ui.common.DefaultNetworkErrorScreen
 import uk.udemy.recordshop.ui.common.DefaultProgressIndicator
@@ -23,13 +27,14 @@ fun ArtistsScreenContent(
     onRefresh: () -> Unit,
     onArtistItemClick: (Long) -> Unit
 ) {
-    when(state) {
+    when (state) {
         is ArtistsViewModel.State.Error -> {
             DefaultErrorScreen(
                 responseCode = state.responseCode ?: 0,
                 errorMessage = state.errorMessage
             )
         }
+
         is ArtistsViewModel.State.Loaded -> {
             ArtistsScreenLoaded(
                 state = state,
@@ -38,9 +43,11 @@ fun ArtistsScreenContent(
                 onArtistItemClick = onArtistItemClick
             )
         }
+
         ArtistsViewModel.State.Loading -> {
             DefaultProgressIndicator()
         }
+
         is ArtistsViewModel.State.NetworkError -> {
             DefaultNetworkErrorScreen(
                 errorMessage = state.errorMessage
@@ -55,7 +62,7 @@ fun ArtistsScreenLoaded(
     pullToRefreshState: PullToRefreshState,
     onRefresh: () -> Unit,
     onArtistItemClick: (Long) -> Unit
-){
+) {
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -64,15 +71,22 @@ fun ArtistsScreenLoaded(
                 state = pullToRefreshState,
                 onRefresh = onRefresh
             ),
-    ){
-        Text("Artist Screen using ViewModel")
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+        ) {
+            items(state.data) { artistDTO ->
+                ArtistItem(artistDTO = artistDTO, navigateToArtistAlbums = onArtistItemClick)
+            }
+        }
     }
 }
 
-
 @Preview(showBackground = true)
 @Composable
-fun ArtistScreenContentLoadedPreview(){
+fun ArtistScreenContentLoadedPreview() {
     ArtistsScreenContent(
         state = ArtistsViewModel.State.Loaded(),
         pullToRefreshState = rememberPullToRefreshState(),
