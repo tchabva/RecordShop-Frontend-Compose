52233aa5666e4711609a67d15d9248c95636b68f
Tawanda Chabva <tchabva@gmail.com>
2025-04-28 21:27:30 +0100

    feat: implement the design and preview for the ArtistItem

--- app/src/main/java/uk/udemy/recordshop/ui/common/ArtistItem.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/common/ArtistItem.kt
@@ -1,2 +1,49 @@
 package uk.udemy.recordshop.ui.common
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import uk.udemy.recordshop.data.model.ArtistDTO
+
+@Composable
+fun ArtistItem(
+    artistDTO: ArtistDTO,
+    navigateToArtistAlbums: (Long) -> Unit
+) {
+    Row (
+        modifier = Modifier
+            .padding(vertical = 8.dp)
+            .height(56.dp)
+            .fillMaxWidth()
+            .clickable { navigateToArtistAlbums(artistDTO.id) },
+        verticalAlignment = Alignment.CenterVertically
+
+    ) {
+        Text(
+            modifier = Modifier.padding(horizontal = 8.dp),
+            text = artistDTO.name,
+            fontSize = 24.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ArtistItemPreview() {
+    ArtistItem(
+        artistDTO = ArtistDTO(
+            id = 1,
+            name = "Davido"
+        ),
+        navigateToArtistAlbums = {}
+    )
+}
