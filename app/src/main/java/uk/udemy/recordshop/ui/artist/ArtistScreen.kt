12ec8f8b781e9523a5ed1c86b8457e24d65c00bd
Tawanda Chabva <tchabva@gmail.com>
2025-04-29 22:45:37 +0100

    feat: create the ArtistScreen basic clone file

--- app/src/main/java/uk/udemy/recordshop/ui/artist/ArtistScreen.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/artist/ArtistScreen.kt
@@ -1,2 +1,17 @@
 package uk.udemy.recordshop.ui.artist
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ArtistScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text("Artist Screen")
+    }
+}
