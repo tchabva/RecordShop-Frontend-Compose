338791c0d5a2085d93cca97439a326e80c36a8c6
Tawanda Chabva <tchabva@gmail.com>
2025-04-29 15:39:51 +0100

    feat: DefaultNetworkErrorScreen now implements a PullToRefreshBox for layout

--- app/src/main/java/uk/udemy/recordshop/ui/common/DefaultNetworkErrorScreen.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/common/DefaultNetworkErrorScreen.kt
@@ -1,19 +1,27 @@
 package uk.udemy.recordshop.ui.common
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import uk.udemy.recordshop.R
 
 // TODO add to other screens
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DefaultNetworkErrorScreen(errorMessage: String?) {
-    Box(
-        modifier = Modifier.fillMaxSize()
+fun DefaultNetworkErrorScreen(errorMessage: String?, isLoading: Boolean, onRefresh: () -> Unit) {
+    PullToRefreshBox(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        isRefreshing = isLoading,
+        onRefresh = onRefresh
     ) {
         Text(
             modifier = Modifier.align(Alignment.Center),
