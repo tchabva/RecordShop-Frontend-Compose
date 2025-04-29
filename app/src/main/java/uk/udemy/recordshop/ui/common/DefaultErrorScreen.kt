788571dadcb9317063b53425290603e26a0a08fd
Tawanda Chabva <tchabva@gmail.com>
2025-04-29 17:11:46 +0100

    feat: add preview to DefaultErrorScreen and increase size of text and centre-aligned the text

--- app/src/main/java/uk/udemy/recordshop/ui/common/DefaultErrorScreen.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/common/DefaultErrorScreen.kt
@@ -7,6 +7,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import uk.udemy.recordshop.R
 
 // TODO add to other screens
@@ -18,6 +21,8 @@ fun DefaultErrorScreen(responseCode: Int, errorMessage: String?) {
     ) {
         Text(
             modifier = Modifier.align(Alignment.Center),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
             text = stringResource(
                 R.string.error_occurred,
                 responseCode,
@@ -25,4 +30,13 @@ fun DefaultErrorScreen(responseCode: Int, errorMessage: String?) {
             )
         )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultErrorScreenPreview(){
+    DefaultErrorScreen(
+        responseCode = 404,
+        errorMessage = ""
+    )
 }
