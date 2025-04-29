2ea305d6fdb568b9e8764111d5e9bd6f19b4b879
Tawanda Chabva <tchabva@gmail.com>
2025-04-29 16:55:45 +0100

    feat: change PullToRefreshBox to a Column with text and a button to try again
    updated the appearance of the text

--- app/src/main/java/uk/udemy/recordshop/ui/common/DefaultNetworkErrorScreen.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/common/DefaultNetworkErrorScreen.kt
@@ -1,34 +1,45 @@
 package uk.udemy.recordshop.ui.common
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import uk.udemy.recordshop.R
 
 // TODO add to other screens
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DefaultNetworkErrorScreen(errorMessage: String?, isLoading: Boolean, onRefresh: () -> Unit) {
-    PullToRefreshBox(
+fun DefaultNetworkErrorScreen(errorMessage: String?, onTryAgainButtonClicked: () -> Unit) {
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        isRefreshing = isLoading,
-        onRefresh = onRefresh
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            modifier = Modifier.align(Alignment.Center),
+            textAlign = TextAlign.Center,
+            fontSize = 24.sp,
             text = stringResource(
                 R.string.network_error_occurred,
                 errorMessage ?: stringResource(R.string.unknown_error)
             )
         )
+
+        Button(
+            modifier = Modifier.padding(vertical = 16.dp),
+            onClick = onTryAgainButtonClicked
+        ) {
+            Text(text = "Try Again")
+        }
     }
 }
