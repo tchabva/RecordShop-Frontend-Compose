e1fdf7a3c5b08ad1b1633ebe7d605e46be7ff5c5
Tawanda Chabva <tchabva@gmail.com>
2025-08-22 16:19:18 +0100

    feat: add logic for the getAllGenres method in the Genres Repository Impl

--- app/src/main/java/uk/udemy/recordshop/data/repository/GenresRepositoryImpl.kt
+++ app/src/main/java/uk/udemy/recordshop/data/repository/GenresRepositoryImpl.kt
@@ -1,4 +1,44 @@
 package uk.udemy.recordshop.data.repository
 
-class GenresRepositoryImpl {
+import android.util.Log
+import uk.udemy.recordshop.data.model.GenreAndAlbums
+import uk.udemy.recordshop.data.model.GenreDTO
+import uk.udemy.recordshop.data.remote.GenresApi
+import uk.udemy.recordshop.data.remote.NetworkResponse
+import javax.inject.Inject
+
+class GenresRepositoryImpl @Inject constructor(
+    private val api: GenresApi
+) : GenresRepository {
+    override suspend fun getAllGenres(): NetworkResponse<List<GenreDTO>> {
+        try {
+            val response = api.getAllGenres()
+            val responseCode = response.code()
+
+            return if (responseCode == 200) {
+                Log.i(
+                    TAG,
+                    "Successful retrieval of Genres: ${response.body()!!.size} Genres"
+                )
+                NetworkResponse.Success(data = response.body()!!)
+            } else {
+                Log.e(TAG, "Failed Retrieval of Genres: Code = $responseCode")
+                NetworkResponse.Failed(
+                    message = response.message() ?: "",
+                    code = responseCode
+                )
+            }
+        } catch (e: Throwable) {
+            Log.wtf(TAG, "Network Error", e)
+            return NetworkResponse.Exception(exception = e)
+        }
+    }
+
+    override suspend fun getGenreWithAlbums(genreId: Long): NetworkResponse<GenreAndAlbums> {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        private const val TAG = "GenresRepoImpl"
+    }
 }
