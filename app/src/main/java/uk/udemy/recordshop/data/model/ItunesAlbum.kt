608862920896e12cd81b608fff583b1452e3a33e
Tawanda Chabva <tchabva@gmail.com>
2025-08-22 13:55:04 +0100

    refactor: moved the Itunes models to the iTunes file

--- app/src/main/java/uk/udemy/recordshop/data/model/ItunesAlbum.kt
+++
@@ -1,7 +0,0 @@
-package uk.udemy.recordshop.data.model
-
-data class ItunesAlbum(
-    val artistName: String,
-    val collectionName: String,
-    var artworkUrl100: String
-)
--- app/src/main/java/uk/udemy/recordshop/data/model/iTunes.kt
+++ app/src/main/java/uk/udemy/recordshop/data/model/iTunes.kt
@@ -1,2 +1,12 @@
 package uk.udemy.recordshop.data.model
 
+data class ItunesAlbum(
+    val artistName: String,
+    val collectionName: String,
+    var artworkUrl100: String
+)
+
+data class ItunesResponse(
+    val resultCount: Int,
+    val results: List<ItunesAlbum>
+)
