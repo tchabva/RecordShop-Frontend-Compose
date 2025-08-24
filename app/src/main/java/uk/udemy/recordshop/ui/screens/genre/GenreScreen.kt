08339f394587cb24c83260cbde9510cdbb1e6fa5
Tawanda Chabva <tchabva@gmail.com>
2025-08-24 23:43:05 +0100

    feat: create the Genre package and placeholder screen

---
+++ app/src/main/java/uk/udemy/recordshop/ui/screens/genre/GenreScreen.kt
@@ -0,0 +1,17 @@
+package uk.udemy.recordshop.ui.screens.genre
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun GenreScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text("Genres Screen")
+    }
+}
