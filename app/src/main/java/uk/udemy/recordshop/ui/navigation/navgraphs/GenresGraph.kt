ad0f22540ec0749aa27e39884696bd4851fc4da7
Tawanda Chabva <tchabva@gmail.com>
2025-08-24 23:48:42 +0100

    feat: implement the GenresScreen in the GenresGraph

--- app/src/main/java/uk/udemy/recordshop/ui/navigation/navgraphs/GenresGraph.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/navigation/navgraphs/GenresGraph.kt
@@ -1,19 +1,41 @@
 package uk.udemy.recordshop.ui.navigation.navgraphs
 
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
-import uk.udemy.recordshop.ui.screens.genres.GenresScreen
+import androidx.navigation.toRoute
 import uk.udemy.recordshop.ui.navigation.Screens
 import uk.udemy.recordshop.ui.navigation.Tabs
+import uk.udemy.recordshop.ui.screens.genre.GenreScreen
+import uk.udemy.recordshop.ui.screens.genres.GenresScreen
+import uk.udemy.recordshop.ui.screens.genres.GenresViewModel
 
 fun NavGraphBuilder.genresGraph(
     navController: NavController
 ) {
     navigation<Tabs.Genres>(startDestination = Screens.Genres) {
         composable<Screens.Genres> {
-            GenresScreen()
+            GenresScreen(
+                viewModel = hiltViewModel<GenresViewModel>(),
+                onGenreItemClicked = { genreId ->
+                    // Navigates to the Genre Screen with the genreId
+                    navController.navigate(Screens.Genre(genreId))
+                }
+            )
+        }
+
+        composable<Screens.Genre> { backStackEntry ->
+            val genreScreen: Screens.Genre = backStackEntry.toRoute()
+//            val viewModel = hiltViewModel<GenreViewModel>()
+
+            LaunchedEffect(genreScreen.genreId) {
+//                viewModel.getGenreWithAlbums(genreId = genreScreen.genreId)
+            }
+
+            GenreScreen()
         }
     }
 }
