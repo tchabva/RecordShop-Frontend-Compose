808627ac903fc41354e9e96da1fc464412f120eb
Tawanda Chabva <tchabva@gmail.com>
2025-08-25 00:12:37 +0100

    feat: create GrenresScreenContent file

--- app/src/main/java/uk/udemy/recordshop/ui/screens/genres/GenresScreenContent.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/screens/genres/GenresScreenContent.kt
@@ -1,2 +1,47 @@
 package uk.udemy.recordshop.ui.screens.genres
 
+import androidx.compose.runtime.Composable
+import uk.udemy.recordshop.ui.common.DefaultErrorScreen
+import uk.udemy.recordshop.ui.common.DefaultNetworkErrorScreen
+import uk.udemy.recordshop.ui.common.DefaultProgressIndicator
+
+@Composable
+fun GenresScreensContent(
+    state: GenresViewModel.State,
+    onRefresh: () -> Unit,
+    onGenreItemClicked: (Long) -> Unit,
+    onTryAgainButtonClicked: () -> Unit
+) {
+    when (state) {
+
+        is GenresViewModel.State.Error -> {
+            DefaultErrorScreen(
+                responseCode = state.responseCode ?: 0,
+                errorMessage = state.errorMessage
+            )
+        }
+        is GenresViewModel.State.Loaded -> {
+            GenresScreenLoaded(
+
+            )
+        }
+        GenresViewModel.State.Loading -> {
+            DefaultProgressIndicator()
+        }
+        is GenresViewModel.State.NetworkError -> {
+            DefaultNetworkErrorScreen(
+                errorMessage = state.errorMessage,
+                onTryAgainButtonClicked = onTryAgainButtonClicked
+            )
+        }
+    }
+}
+
+@Composable
+fun GenresScreenLoaded(
+    state: GenresViewModel.State.Loaded,
+    onRefresh: () -> Unit,
+    onGenreItemClicked: (Long) -> Unit
+) {
+    
+}
