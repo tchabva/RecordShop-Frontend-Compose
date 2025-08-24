309c4c10d25588a8001c89998fd91bc7175b7aaa
Tawanda Chabva <tchabva@gmail.com>
2025-08-25 00:19:12 +0100

    feat: implement the Genres Screen

--- app/src/main/java/uk/udemy/recordshop/ui/screens/genres/GenresScreenContent.kt
+++ app/src/main/java/uk/udemy/recordshop/ui/screens/genres/GenresScreenContent.kt
@@ -1,9 +1,17 @@
 package uk.udemy.recordshop.ui.screens.genres
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import uk.udemy.recordshop.ui.common.DefaultErrorScreen
 import uk.udemy.recordshop.ui.common.DefaultNetworkErrorScreen
 import uk.udemy.recordshop.ui.common.DefaultProgressIndicator
+import uk.udemy.recordshop.ui.common.GenreItem
 
 @Composable
 fun GenresScreensContent(
@@ -13,21 +21,25 @@ fun GenresScreensContent(
     onTryAgainButtonClicked: () -> Unit
 ) {
     when (state) {
-
         is GenresViewModel.State.Error -> {
             DefaultErrorScreen(
                 responseCode = state.responseCode ?: 0,
                 errorMessage = state.errorMessage
             )
         }
+
         is GenresViewModel.State.Loaded -> {
             GenresScreenLoaded(
-
+                state = state,
+                onRefresh = onRefresh,
+                onGenreItemClicked = onGenreItemClicked
             )
         }
+
         GenresViewModel.State.Loading -> {
             DefaultProgressIndicator()
         }
+
         is GenresViewModel.State.NetworkError -> {
             DefaultNetworkErrorScreen(
                 errorMessage = state.errorMessage,
@@ -43,5 +55,21 @@ fun GenresScreenLoaded(
     onRefresh: () -> Unit,
     onGenreItemClicked: (Long) -> Unit
 ) {
-    
+    PullToRefreshBox(
+        isRefreshing = state.isLoading,
+        onRefresh = onRefresh
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+        ) {
+            items(state.data) { genreDTO ->
+                GenreItem(
+                    genreDTO = genreDTO,
+                    navigateToGenreAlbums = onGenreItemClicked
+                )
+            }
+        }
+    }
 }
