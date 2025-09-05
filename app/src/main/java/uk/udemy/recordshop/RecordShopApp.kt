e4d2a1c9f3f138cfd2ee7f504c89d3e329c41c54
Tawanda Chabva <tchabva@gmail.com>
2025-09-05 21:50:53 +0100

    remove git stash

--- app/src/main/java/uk/udemy/recordshop/RecordShopApp.kt
+++
@@ -1,7 +0,0 @@
-package uk.udemy.recordshop
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class RecordShopApp: Application()
--- app/src/main/java/uk/udemy/recordshop/data/repository/ItunesRepository.kt
+++
@@ -1,8 +0,0 @@
-package uk.udemy.recordshop.data.repository
-
-import uk.udemy.recordshop.data.model.ItunesAlbum
-import uk.udemy.recordshop.data.remote.NetworkResponse
-
-interface ItunesRepository {
-    suspend fun getAlbumArtwork(searchQuery: String): NetworkResponse<ItunesAlbum>
-}
